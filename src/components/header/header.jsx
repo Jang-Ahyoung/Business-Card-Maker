f5c495358048bf15d70a6d77a1cbbed4c742eaa8
Jang-Ahyoung <wegotthelove27@gmail.com>
2020-12-25 21:13:14 +0900

    add login component css

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = (props) => (
-    <header >
-        <div className={styles.box}>
+const Header = ({onLogout}) => (
+// const Header = (props) => (
+    <header  className={styles.box}>
+       {/* onLogout해주거나 true해주면돼 항상 보여지게돼!*/}
             <img className={styles.logo} src="/images/logo.png" alt="logo"/>
-    <p className={styles.title}>Business Card Maker</p>
+            <p className={styles.title}>Business Card Maker</p>
+            {onLogout && (<button className={styles.logout} onClick={onLogout}>Logout</button>)}
 
-        </div>
+
         </header>
 );
 
